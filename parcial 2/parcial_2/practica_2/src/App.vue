163966cbb6892ba804d6e4178ae65e5a7a4f4735
espinosa34 <genaro1546@hotmail.com>
2018-11-23 19:46:14 -0500

    practica

--- parcial 2/parcial_2/practica_2/src/App.vue
+++ parcial 2/parcial_2/practica_2/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>practica de inventario de ({ejemplo})</h1>
+    <el-radio v-model="radio" label="1">Option A</el-radio>
+   <el-radio v-model="radio" label="2">Option B</el-radio>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+   export default {
+    data () {
+      return {
+        radio: '1'
+      };
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
