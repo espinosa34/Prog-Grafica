a3375108b36e21f138f12191899a454b75aa9d71
espinosa34 <genaro1546@hotmail.com>
2018-12-07 20:44:10 -0500

    practica

---
+++ practica_3/src/components/prueba.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1> Componente prueba</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'prueba',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
---
+++ practica_3/src/components/visualizar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Listado de inventario de libros</h1>
+        <p v-for="(libro, index) in inventario" :key="index"> {{ libro }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'visualizar',
+    data() {
+        return {
+            inventario: [
+                'El secreto',
+                'Romen y Julieta',
+                'La sombra del viento',
+                'Los juegos del hambre',
+                'Harry Potter',
+                'Inteligencia',
+                'La vaca',
+                'El alquimista',
+                'La mancha del quijote',
+                'Padre rico Padre Pobre',
+                'La biblia'
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
