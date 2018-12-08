4dfdeabfa3430c337f1bc1a62a932c0025dc88b5
espinosa34 <genaro1546@hotmail.com>
2018-12-07 20:16:45 -0500

    vue

--- practica_3/src/App.vue
+++ practica_3/src/App.vue
@@ -1,16 +1,16 @@
 
 <template>
   <div id="app">
-    <el-row>
-      <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
-    </el-row>
+    
+    <br><br>
+
   </div>
 </template>
 
 <script>
 
 export default {
-
+  
 }
 </script>
 
