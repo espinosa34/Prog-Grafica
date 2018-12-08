1472f739df4e1d69aefba08de932c7b0d1e8b07c
espinosa34 <genaro1546@hotmail.com>
2018-12-07 20:53:46 -0500

    vue

--- practica_3/src/App.vue
+++ practica_3/src/App.vue
@@ -1,16 +1,23 @@
-
 <template>
   <div id="app">
-    
-    <br><br>
-
+      <h1>Practica de Inventario de Libros</h1>
+      <!-- <Visualizar/> -->
+      <producto/>
   </div>
 </template>
 
 <script>
+import Visualizar from './components/Visualizar.vue'
+import inventario from './components/inventario.vue'
+import inventario_2 from './components/inventario_2.vue'
+import prueba from './components/prueba.vue'
 
 export default {
-  
+  name: 'app',
+  components: {
+    Visualizar,
+    inventario
+  }
 }
 </script>
 
